7a815029275cacd0e7067d7a71b068e3588b93c7
Joao Francisco Perpectuo Figueiredo <jperpect@c1r14s1.42porto.com>
2024-04-24 14:28:24 +0100

    24/5/2024_14:28

--- libft/srcs/ft_lstadd_front.c
+++ libft/srcs/ft_lstadd_front.c
@@ -1,36 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jperpect <jperpect@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/16 12:17:02 by jperpect          #+#    #+#             */
-/*   Updated: 2024/04/24 11:01:36 by jperpect         ###   ########.fr       */
+/*   Created: 2024/04/24 11:09:48 by jperpect          #+#    #+#             */
+/*   Updated: 2024/04/24 11:57:35 by jperpect         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-void *ft_calloc(size_t nmemb, size_t size)
+
+void ft_lstadd_front(t_list **lst, t_list *new)
 {
-    void *ret;
-    ret = (void *)malloc(nmemb * size);
-    if (ret == NULL)
-    return NULL;
-    ft_bzero(ret,nmemb * size);
-    return(ret);
+	if(lst == NULL || new == NULL)
+	 return;
+	new -> next = *lst; 
+	*lst = new;
 }
 
-// int main(void) 
+
+// int main(int ac,char **av)
 // {
-//     char *ok = ft_calloc(2147483647,45);
-
-//     printf("%s\n",ok); 
-//     char *oks = calloc(2147483647,45);
-//     //ft_memset(oks,'a',10);
-
-//     printf("%s",oks);
-    
-
+// 	t_list *ok = ft_lstnew(av[1]);
+// 	t_list *oks = ft_lstnew(av[2]);
+	
+	
+	
+// 	ft_lstadd_front(&ok ,oks);
+	
+// 	ft_putstr_fd(ok->content,1);
+// 	ok = ok->next;
+// 	ft_putstr_fd(ok->content,1);
+	
 // }
-
--- libft/srcs/libft.h
+++ libft/srcs/libft.h
@@ -6,64 +6,59 @@
 /*   By: jperpect <jperpect@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 13:36:29 by jperpect          #+#    #+#             */
-/*   Updated: 2024/04/22 11:08:21 by jperpect         ###   ########.fr       */
+/*   Updated: 2024/04/24 14:07:46 by jperpect         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
-#define LIBFT_H
+# define LIBFT_H
 
-#include <unistd.h>
-#include <aio.h>
-#include <stdlib.h>
+# include <aio.h>
+# include <bsd/string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <strings.h>
+# include <unistd.h>
 
-#include <stdio.h>
-#include <strings.h>
-#include <string.h>
-#include <bsd/string.h>
+int					ft_isalnum(int c);
+int					ft_isalpha(int c);
+int					ft_isascii(int c);
+int					ft_isdigit(int c);
+int					ft_isprint(int c);
 
-
-
-
-int ft_isalnum(int c);
-int	ft_isalpha(int c);
-int ft_isascii(int c);
-int	ft_isdigit(int c);
-int ft_isprint(int c);
-
-
-
-void *ft_memmove(void *dest, const void *src, size_t n);
+void				*ft_memmove(void *dest, const void *src, size_t n);
 /**
 * @brief move de bitts of the memori to destany.
 * @param dest variavel that get bitts.
 * @param src  variavel with bitts to move.
 * @return void variabel dest.
 */
-void *ft_memcpy(void *dest, const void *src, size_t n);
+void				*ft_memcpy(void *dest, const void *src, size_t n);
 
-void	*ft_memset(void *s, int c, size_t n);
-void ft_bzero(void *s, size_t n);
-size_t ft_strlen(const char *s);
-size_t ft_strlcpy(char *dst, const char *src, size_t size);
-size_t ft_strlcat(char *dst, const char *src, size_t size);
-int ft_tolower(int c);
-int ft_toupper(int c);
-char *ft_strchr(const char *s, int c);
-char *ft_strrchr(const char *s, int c);
-int ft_strncmp(const char *s1, const char *s2, size_t n);
-void *ft_memchr(const void *s, int c, size_t n);
-int ft_memcmp(const void *s1, const void *s2, size_t n);
-char *ft_strnstr(const char *big, const char *little, size_t len);
+void				*ft_memset(void *s, int c, size_t n);
+void				ft_bzero(void *s, size_t n);
+size_t				ft_strlen(const char *s);
+size_t				ft_strlcpy(char *dst, const char *src, size_t size);
+size_t				ft_strlcat(char *dst, const char *src, size_t size);
+int					ft_tolower(int c);
+int					ft_toupper(int c);
+char				*ft_strchr(const char *s, int c);
+char				*ft_strrchr(const char *s, int c);
+int					ft_strncmp(const char *s1, const char *s2, size_t n);
+void				*ft_memchr(const void *s, int c, size_t n);
+int					ft_memcmp(const void *s1, const void *s2, size_t n);
+char				*ft_strnstr(const char *big, const char *little,
+						size_t len);
 
-int ft_atoi(const char *nptr);
+int					ft_atoi(const char *nptr);
 /**
 * @brief convert char in int.
 * @param nptr nuber in variabel char. 
 * @return nuber in variabel int. 
 */
 
-void *ft_calloc(size_t nmemb, size_t size);
+void				*ft_calloc(size_t nmemb, size_t size);
 /**
 * @brief create a variable with memory reserved.
 * @param nemeb number de menbers os de valiabel. 
@@ -71,14 +66,14 @@ void *ft_calloc(size_t nmemb, size_t size);
 * @return variabel zered .
 */
 
-char *ft_strdup(const char *s);
+char				*ft_strdup(const char *s);
 /**
 * @brief copy a sting to new string.
 * @param s setring to copy.
 * @return string compyed. 
 */
 
-char *ft_substr(char const *s, unsigned int start,size_t len);
+char				*ft_substr(char const *s, unsigned int start, size_t len);
 /**
 * @brief copy  string  as of start.
 * @param s setring inicals.
@@ -87,7 +82,7 @@ char *ft_substr(char const *s, unsigned int start,size_t len);
 * @return substring inicialeze in.
 */
 
-char *ft_strjoin(char const *s1, char const *s2);
+char				*ft_strjoin(char const *s1, char const *s2);
 /**
 * @brief concatenation of two strings. 
 * @param s1 first string.
@@ -95,15 +90,15 @@ char *ft_strjoin(char const *s1, char const *s2);
 * @return reutnr a concatenation of s1 and s2.
 */
 
-char *ft_strtrim(char const *s1, char const *set);
+char				*ft_strtrim(char const *s1, char const *set);
 /**
-* @brief remuve *set caraters up to meet caracter difrent to set caracter. end and satrt.
+* @brief remuve *set caraters up to meet it difrent to set caracter.
 * @param s1 string complit;
 * @param set caracters to remuve;
 * @return a string without os cararteres de set 
 */
 
-char **ft_split(char const *s, char c);
+char				**ft_split(char const *s, char c);
 /**
 * @brief separite a string in words baseated in caractre c.
 * @param s string to division. 
@@ -111,61 +106,90 @@ char **ft_split(char const *s, char c);
 * @return list separated in word
 */
 
-char *ft_itoa(int n);
+char				*ft_itoa(int n);
 /**
 * @brief tranform int to char. 
 * @param n number in int.
 * @return char wtid memory aloceted. 
 */
 
-char *ft_strmapi(char const *s, char (*f)(unsigned int, char));
+char				*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 /**
 * @brief excute a funcion f in all elements of s.
 * @param s strimg where aplicate a funcion .
 * @param f funcion that get unsigned int and char.
 */
 
-void ft_striteri(char *s, void (*f)(unsigned int,char*));
+void				ft_striteri(char *s, void (*f)(unsigned int, char *));
 /**
 * @brief excute a funcion f in all elements of s.
 * @param s strimg where aplicate a funcion .
 * @param f funcion that get unsigned int and char*.
 */
 
-void ft_putchar_fd(char c, int fd);
+void				ft_putchar_fd(char c, int fd);
 /**
 * @brief display a char c in terminal
 * @param c char display.
 * @param fd filed
 */
 
-void ft_putstr_fd(char *s, int fd);
+void				ft_putstr_fd(char *s, int fd);
 /**
 * @brief display a string s in terminal
 * @param s char display.
 * @param fd filed
 */
 
-void ft_putendl_fd(char *s, int fd);
+void				ft_putendl_fd(char *s, int fd);
 /**
 * @brief display a string s in terminal next new line
 * @param s char display.
 * @param fd filed
 */
-void ft_putnbr_fd(int n, int fd);
+void				ft_putnbr_fd(int n, int fd);
 /**
 * @brief display a int n in terminal
 * @param n int displayed.
 * @param fd filed.
 */
+
+
 typedef struct s_list
 {
-void *content;
-struct s_list *next;
-} t_list;
+	void			*content;
+	struct s_list	*next;
+}					t_list;
 
+
+t_list *ft_lstnew(void *content);
+/**
+* @brief creiate a new node .
+* @param content variavel content to new node. 
+* @return the new node to memory allocate.
+*/
+void ft_lstadd_front(t_list **lst, t_list *new);
+/**
+* @brief add new menber tu list.
+* @param lst list existing. 
+* @param new elmente to list.
+*/
+int ft_lstsize(t_list *lst);
+/**
+* @brief count nuber to elementes to list
+* @param lst list in first elemnte
+* @return number of elementes 
+*/
+t_list *ft_lstlast(t_list *lst);
+/**
+* @brief lacte to last node to list.
+* @param lst node in first elemente.
+* @return last node. 
+*/
+void ft_lstadd_back(t_list **lst, t_list *new);
 
 #endif
+
 /**
      * @brief Subtracts one number from another.
      * @param a The first number.
